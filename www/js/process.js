1a9f93eeee99263a7f7970efff3f18057a111494
dianvirnadethbiag <dianvirnadethbiag@gmail.com>
2017-12-12 15:25:05 +0800

    final app

---
+++ www/js/process.js
@@ -0,0 +1,431 @@
+let process = {
+	menu:function(){
+		let content = 
+		<div><center><h1>Music and Lyrics</h1><h2>Song List</h2>
+			<div><a href='#' class="button button-big button-fill button-raised color-green" onClick={process.song1}>So Sick</a></div>
+			<div><a href='#' class="button button-big button-none button-raised color-danger" onClick={process.song2}>One Call Away</a></div>
+			<div><a href='#' class="button button-big button-fill button-raised color-teal" onClick={process.song3}>Panaginip Lang</a></div>	
+			<div><a href='#' class="button button-big button-none button-raised color-teal" onClick={process.song4}>My Love</a></div>
+			<div><a href='#' class="button button-big button-fill button-raised color-cyan" onClick={process.song5}>Perfect</a></div>
+			
+
+		</center></div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	song1:function(){
+		let content =
+		<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+            
+
+				<div><center><audio controls><source src="audio/so sick.mp3" type="audio/mpeg" /></audio></center></div>
+				
+			
+<h3>So Sick</h3>
+
+<p>By: Ne-Yo</p>
+<div>
+<p>Do do do do do do do-do</p>
+<p>Ohh Yeah</p>
+
+<p>[Verse 1]</p>
+<p>Gotta change my answering machine</p>
+<p>Now that I'm alone</p>
+<p>'Cause right now it says that we</p>
+<p>Can't come to the phone</p>
+<p>And I know it makes no sense</p>
+<p>'Cause you walked out the door</p>
+<p>But it's the only way I hear your voice anymore</p>
+<p>(it's ridiculous)</p>
+<p>It's been months</p>
+<p>And for some reason I just</p>
+<p>(can't get over us)</p>
+<p>And I'm stronger than this</p>
+<p>(enough is enough)</p>
+<p>No more walking round</p>
+<p>With my head down</p>
+<p>I'm so over being blue</p>
+<p>Crying over you</p>
+<p>[Hook]</p>
+<p>And I'm so sick of love songs</p>
+<p>So tired of tears</p>
+<p>So done with wishing you were still here</p>
+<p>Said I'm so sick of love songs so sad and slow</p>
+<p>So why can't I turn off the radio?</p>
+
+<p>[Verse 2]</p>
+<p>Gotta fix that calendar I have</p>
+<p>That's marked July 15th</p>
+<p>Because since there's no more you</p>
+<p>There's no more anniversary</p>
+<p>I'm so fed up with my thoughts of you</p>
+<p>And your memory</p>
+<p>And how every song reminds me</p>
+<p>Of what used to be</p>
+<p>That's the reason</p>
+
+<p>[Hook]</p>
+<p>And I'm so sick of love songs</p>
+<p>So tired of tears</p>
+<p>So done with wishing you were still here</p>
+<p>Said I'm so sick of love songs so sad and slow</p>
+<p>So why can't I turn off the radio?</p>
+
+<p>[Bridge]</p>
+<p>(Leave me alone)</p>
+<p>Leave me alone</p>
+<p>(Stupid love songs)</p>
+<p>Don't make me think about her smile</p>
+<p>Or having my first child</p>
+<p>I'm letting go</p>
+<p>Turning off the radio</p>
+
+<p>[Hook]</p>
+<p>And I'm so sick of love songs</p>
+<p>So tired of tears</p>
+<p>So done with wishing you were still here</p>
+<p>Said I'm so sick of love songs so sad and slow</p>
+<p>So why can't I turn off the radio?</p>
+<p>(why can't I turn off the radio?)</p>
+<p>And I'm so sick of love songs</p>
+<p>So tired of tears</p>
+<p>So done with wishing you were still here</p>
+<p>Said I'm so sick of love songs so sad and slow</p>
+<p>So why can't I turn off the radio?</p>
+<p>(why can't I turn off the radio?)</p>
+<p>And I'm so sick of love songs</p>
+<p>So tired of tears</p>
+<p>So done with wishing you were still here</p>
+<p>Said I'm so sick of love songs so sad and slow</p>
+<p>So why can't I turn off the radio?</p>
+
+<p>[Outro]</p>
+<p>Why can't I turn off the radio?</p>
+<p>Why can't I turn off the radio?</p>
+<p>***********************************</p>
+</div>
+
+<a href='#' class="button button-big button-fill button-raised color-blue gray" onClick={process.menu}>BACK</a>
+				
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	song2:function(){
+		let content = 
+	<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+
+				<div><center><audio controls><source src="audio/one call away.mp3" type="audio/mpeg" /></audio></center></div>
+				<p>_*_*_*_</p>
+				<h4>One Call Away</h4>
+				<p>By: Charlie Puth</p>
+
+			<div>
+				<p>I'm only one call away</p>
+				<p>I'll be there to save the day</p>
+				<p>Superman got nothing on me</p>
+				<p>I'm only one call away</p>
+				<p>Call me, baby, if you need a friend</p>
+				<p>I just wanna give you love</p>
+				<p>Come on, come on, come on</p>
+				<p>Reaching out to you, so take a chance</p>
+				<p>No matter where you go</p>
+				<p>You know you're not alone</p>
+				<p>I'm only one call away</p>
+				<p>I'll be there to save the day</p>
+				<p>Superman got nothing on me</p>
+				<p>I'm only one call away</p>
+				<p>Come along with me and don't be scared</p>
+				<p>I just wanna set you free</p>
+				<p>Come on, come on, come on</p>
+				<p>You and me can make it anywhere</p>
+				<p>For now, we can stay here for a while, ay</p>
+				<p>'Cause you know, I just wanna see you smile</p>
+				<p>No matter where you go</p>
+				<p>You know you're not alone</p>
+				<p>I'm only one call away</p>
+				<p>I'll be there to save the day</p>
+				<p>Superman got nothing on me</p>
+				<p>I'm only one call away</p>
+				<p>And when you're weak I'll be strong</p>
+				<p>I'm gonna keep holding on</p>
+				<p>Now don't you worry, it won't be long, Darling</p>
+				<p>And when you feel like hope is gone</p>
+				<p>Just run into my arms</p>
+				<p>I'm only one call away</p>
+				<p>I'll be there to save the day</p>
+				<p>Superman got nothing on me</p>
+				<p>I'm only one, I'm only one call away</p>
+				<p>I'll be there to save the day</p>
+				<p>Superman got nothing on me</p>
+				<p>I'm only one call away</p>
+				<p>I'm only one call away</p>
+				<p>***********************************</p>
+
+				<a href='#' class="button button-big button-fill button-raised color-blue gray" onClick={process.menu}>BACK</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	song3:function(){
+		let content = 
+	<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+
+				
+				<div><center><audio controls><source src="audio/panaginip lang.mp3" type="audio/mpeg" /></audio></center></div>
+				<p>_*_*_*_</p>
+				<h5>Panaginip Lang</h5>
+				<p>By: Alex Gonzaga</p>
+			<div>
+				<p>Panaginip Lang Ba nung ika’y nakilala</p>
+				<p>Katabi sa Klase chikahan ang naging hobby</p>
+				<p>Panaginip lang ba nung ika’y napatawa</p>
+				<p>Sa matalik na kaibigan</p>
+				<p>Nang hindi namamalayan</p>
+				<p>Refrain:</p>
+				<p>Hindi ko inakalang</p>
+				<p>Doon pala nagsimula, love story’ng hindi inaasahan</p>
+				<p>Sa huli tayo pala</p>
+				<p>Sa isa’t isa….</p>
+				<p>Chorus:</p>
+				<p>Magkahawak ang mga kamay</p>
+				<p>Magkasabay sa lakad ng buhay</p>
+				<p>Ang araw at ang buwan na ang nagsasabing</p>
+				<p>Ako’y sa’yo lamang at</p>
+				<p>Ikaw ay para sakin Lamang</p>
+				<p>Parang panaginip lamang </p>
+				<p>Parang panaginip lamang</p>
+				<p>Verse II</p>
+				<p>Panaginip lang ba nung ika’y nakasama</p>
+				<p>Sa ating Eskwela at ikaw ay</p>
+				<p>Pumuporma</p>
+				<p>Isa lang ang nasa isip at wala nang iba</p>
+				<p>Gulat ako noong sinabi mong mahal na mahal kita</p>
+				<p>Refrain:</p>
+				<p>Hindi ko inakalang</p>
+				<p>Doon pala nagsimula, love story’ng hindi inaasahan</p>
+				<p>Sa huli tayo pala</p>
+				<p>Sa isa’t isa….</p>
+				<p>Chorus:</p>
+				<p>Magkahawak ang mga kamay</p>
+				<p>Magkasabay sa lakad ng buhay</p>
+				<p>Ang araw at ang buwan na ang nagsasabing</p>
+				<p>Ako’y sa’yo lamang at</p>
+				<p>Ikaw ay para sakin lamang</p>
+				<p>Parang panaginip lamang </p>
+				<p>Parang panaginip lamang </p>
+				<p>[Bridge]</p>
+				<p>Para Pan Para Pan Parang</p>
+				<p>Panaginip lamang... </p>
+				<p>Para Pan Para Pan Parang</p>
+				<p>Panaginip lang... </p>
+				<p>Para Pan Para Pan Parang</p>
+				<p>Panaginip lamang... </p>
+				<p>Verse III</p>
+				<p>Panaginip lang ba nung ikaw ay nayakap</p>
+				<p>Madilim ang ulap at ang langit ay umiiyak</p>
+				<p>Pawis sa palad sumasama na sa luha at ulan</p>
+				<p>Nung sinabi kong mahal na rin kita</p>
+				<p>Refrain:</p>
+				<p>Hindi ko inakalang</p>
+				<p>Doon pala nagsimula, love story’ng hindi inaasahan</p>
+				<p>Sa huli tayo pala</p>
+				<p>Sa isa’t isa….</p>
+				<p>Chorus:</p>
+				<p>Magkahawak ang mga kamay</p>
+				<p>Magkasabay sa lakad ng buhay</p>
+				<p>Ang araw at ang buwan na ang nagsasabing</p>
+				<p>Ako’y sa’yo lamang at</p>
+				<p>Ikaw ay para sakin Lamang</p>
+				<p>Parang panaginip lamang …</p>
+				<p>Parang panaginip lamang …</p>
+				<p>Para Pan Para Pan Parang</p>
+				<p>Panaginip lang... </p>
+				<p>Para Pan Para Pan</p>
+				<p>Parang panaginip</p>
+				<p>Ohhhhhhhhhhhh(4x)</p>
+				<p>Para Pan Para Pan</p>
+				<p>Parang panaginip(2x)</p>
+				<p>***********************************</p>
+
+				<a href='#' class="button button-big button-fill button-raised color-blue gray" onClick={process.menu}>BACK</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	song4:function(){
+		let content = 
+	<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+
+				
+				<div><center><audio controls><source src="audio/my love.mp3" type="audio/mpeg" /></audio></center></div>
+				<p>_*_*_*_</p>
+				<h6>My Love</h6>
+				<p>By: Westlife</p>
+			<div>
+				<p>An empty street, an empty house</p>
+				<p>A hole inside my heart</p>
+				<p>I'm all alone, the rooms are getting smaller</p>
+				<p>I wonder how, I wonder why</p>
+				<p>I wonder where they are</p>
+				<p>The days we had, the songs we sang together</p>
+				<p>Oh yeah</p>
+				<p>And oh, my love</p>
+				<p>I'm holding on forever</p>
+				<p>Reaching for the love that seems so far</p>
+				<p>So I say a little prayer</p>
+				<p>And hope my dreams will take me there</p>
+				<p>Where the skies are blue</p>
+				<p>To see you once again, my love</p>
+				<p>Overseas from coast to coast</p>
+				<p>To find a place I love the most</p>
+				<p>Where the fields are green</p>
+				<p>To see you once again, my love</p>
+				<p>I try to read, I go to work</p>
+				<p>I'm laughing with my friends</p>
+				<p>But I can't stop, to keep myself from thinking, oh no</p>
+				<p>I wonder how, I wonder why</p>
+				<p>I wonder where they are</p>
+				<p>The days we had the songs, we sang together</p>
+				<p>Oh yeah</p>
+				<p>And oh, my love</p>
+				<p>I'm holding on forever</p>
+				<p>Reaching for the love that seems so far</p>
+				<p>So I say a little prayer</p>
+				<p>And hope my dreams will take me there</p>
+				<p>Where the skies are blue</p>
+				<p>To see you once again, my love</p>
+				<p>Overseas from coast to coast</p>
+				<p>To find a place I love the most</p>
+				<p>Where the fields are green</p>
+				<p>To see you once again</p>
+				<p>To hold you in my arms</p>
+				<p>To promise you my love</p>
+				<p>To tell you from the heart</p>
+				<p>You're all I'm thinking of</p>
+				<p>I'm reaching for the love that seem so far</p>
+				<p>(So), So I say a little prayer</p>
+				<p>And hope my dreams will take me there</p>
+				<p>Where the skies are blue</p>
+				<p>To see you once again, my love</p>
+				<p>Overseas from coast to coast</p>
+				<p>To find a place I love the most</p>
+				<p>Where the fields are green</p>
+				<p>To see you once again (my love)</p>
+				<p>So I say a little prayer</p>
+				<p>And hope my dreams will take me there</p>
+				<p>Where the skies are blue</p>
+				<p>To see you once again, (Oh, my love)</p>
+				<p>Overseas from coast to coast</p>
+				<p>To find a place I love the most</p>
+				<p>Where the fields are green</p>
+				<p>To see you once again, my love</p>
+				<p>***********************************</p>
+
+				<a href='#' class="button button-big button-fill button-raised color-blue gray" onClick={process.menu}>BACK</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+
+
+	},
+	song5:function(){
+		let content = 
+	<div class="views">
+            <div class="view view-main">
+                <div class="pages">
+					<div class="page-content">
+
+				<div><center><audio controls><source src="audio/perfect.mp3" type="audio/mpeg" /></audio></center></div>
+				<p>_*_*_*_</p>
+				<h6>Perfect</h6>
+				<p>By: Ed Sheeran</p>
+			<div>
+				<p>I found a love for me</p>
+				<p>Darling just dive right in</p>
+				<p>And follow my lead</p>
+				<p>Well I found a girl beautiful and sweet</p>
+				<p>I never knew you were the someone waiting for me</p>
+				<p>'Cause we were just kids when we fell in love</p>
+				<p>Not knowing what it was</p>
+				<p>I will not give you up this time</p>
+				<p>But darling, just kiss me slow, your heart is all I own</p>
+				<p>And in your eyes you're holding mine</p>
+				<p>Baby, I'm dancing in the dark with you between my arms</p>
+				<p>Barefoot on the grass, listening to our favorite song</p>
+				<p>When you said you looked a mess, I whispered underneath my breath</p>
+				<p>But you heard it, darling, you look perfect tonight</p>
+				<p>Well I found a woman, stronger than anyone I know</p>
+				<p>She shares my dreams, I hope that someday I'll share her home</p>
+				<p>I found a love, to carry more than just my secrets</p>
+				<p>To carry love, to carry children of our own</p>
+				<p>We are still kids, but we're so in love</p>
+				<p>Fighting against all odds</p>
+				<p>I know we'll be alright this time</p>
+				<p>Darling, just hold my hand</p>
+				<p>Be my girl, I'll be your man</p>
+				<p>Baby, I'm dancing in the dark with you between my arms</p>
+				<p>I see my future in your eyes</p>
+				<p>Barefoot on the grass, listening to our favorite song</p>
+				<p>When you said you looked a mess, I whispered underneath my breath</p>
+				<p>But you heard it, darling, you look perfect tonight</p>
+				<p>Baby, I'm dancing in the dark, with you between my arms</p>
+				<p>Barefoot on the grass, listening to our favorite song</p>
+				<p>I have faith in what I see</p>
+				<p>Now I know I have met an angel in person</p>
+				<p>And she looks perfect</p>
+				<p>I don't deserve this</p>
+				<p>You look perfect tonight</p>
+				<p>***********************************</p>
+
+				<a href='#' class="button button-big button-fill button-raised color-blue gray" onClick={process.menu}>BACK</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+
+	
+	},
+
+		loading:function(){
+		let content = <div>
+					<i><b>LOADING......</b></i>
+				</div>;
+		ReactDOM.render(content,document.getElementById('root'));
+
+	}
+}
+
+process.loading();
+setTimeout(function(){
+	process.menu();
+},1000);
